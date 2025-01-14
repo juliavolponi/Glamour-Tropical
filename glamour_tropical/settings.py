f9ab43a2d810bdaa8db936a890d4d6868596f879
Julia Volponi <juliavolponi@outlook.com>
2025-01-14 22:50:36 +0000

    fixed issue

--- glamour_tropical/settings.py
+++ glamour_tropical/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$4pe0#o$8o9c8a3^la=us9yi%akn*hcgc*y=hzoo6+$teo!89&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-juliavolpon-glamourtrop-8p8tp3w8zfq.ws.codeinstitute-ide.net'
 ]
